29b15b2a996db786eb868571796423ea19c8f3c9
Michele Barzan <michele@servizioglobale.it>
2021-11-24 17:01:24 +0100

    tastiera login ok

--- connessione.php
+++ connessione.php
@@ -3,8 +3,8 @@
 	$mi_kit_linea_params=json_decode(fread($mi_kit_linea_params_file,filesize("C:\mi_kit_linea_params.json")), true);
 	fclose($mi_kit_linea_params_file);
 
-	$connectionInfo=array("Database"=>"mi_kit_linea", "UID"=>$mi_kit_linea_params['sql_server_info']['username'], "PWD"=>$mi_kit_linea_params['sql_server_info']['password']);
+	$connectionInfo=array("Database"=>"mi_linea_kit", "UID"=>$mi_kit_linea_params['sql_server_info']['username'], "PWD"=>$mi_kit_linea_params['sql_server_info']['password']);
 	$conn = sqlsrv_connect($mi_kit_linea_params['sql_server_info']['ip'],$connectionInfo);
 	if(!$conn)
-		die("error");
+    	die("error " . print_r( sqlsrv_errors(), true));
 ?>
