c7565554e0d4fe348393ef57efa270f05a7f5c63
Michele Barzan <michele@servizioglobale.it>
2021-11-24 15:30:50 +0100

    ok

--- checkPasswordCambiaLineaStazione.php
+++ checkPasswordCambiaLineaStazione.php
@@ -1,8 +1,6 @@
 <?php
 
-    $serverName = '192.168.6.196';
-    $connectionInfo=array("Database"=>"mi_linea_kit", "UID"=>"sa", "PWD"=>"Serglo123");
-    $conn = sqlsrv_connect($serverName,$connectionInfo);
+    include "connessione.php";
 	
     $password= $_POST['password'];
 
--- connessione.php
+++ connessione.php
@@ -1,5 +1,10 @@
 <?php
-$serverName = '192.168.6.195';
-$connectionInfo=array("Database"=>"mi_linea_kit", "UID"=>"sa", "PWD"=>"Serglo123");
-$conn = sqlsrv_connect($serverName,$connectionInfo);
+	$mi_kit_linea_params_file = fopen("C:\mi_kit_linea_params.json", "r") or die("error");
+	$mi_kit_linea_params=json_decode(fread($mi_kit_linea_params_file,filesize("C:\mi_kit_linea_params.json")), true);
+	fclose($mi_kit_linea_params_file);
+
+	$connectionInfo=array("Database"=>"mi_kit_linea", "UID"=>$mi_kit_linea_params['sql_server_info']['username'], "PWD"=>$mi_kit_linea_params['sql_server_info']['password']);
+	$conn = sqlsrv_connect($mi_kit_linea_params['sql_server_info']['ip'],$connectionInfo);
+	if(!$conn)
+		die("error");
 ?>
