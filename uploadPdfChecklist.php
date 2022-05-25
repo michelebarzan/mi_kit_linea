24214a7333a82eeb525a31a3aa92d69db6faf81e
michele barzan <michele@servizioglobale.it>
2022-05-25 14:57:07 +0200

    ok

--- uploadPdfChecklist.php
+++ uploadPdfChecklist.php
@@ -3,7 +3,9 @@
     $target_file = basename($_FILES["file"]["name"]);
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     
-    define ('SITE_ROOT', 'C:\\xampp\\htdocs\\mi_amministrazione_produzione_files\linea_kit\checklist');
+    //$path = "\\\\192.168.6.31\\share";
+    $path = "C:\\xampp\\htdocs\\mi_amministrazione_produzione_files\linea_kit\checklist";
+    define ('SITE_ROOT', $path);
 
     if (!move_uploaded_file($_FILES["file"]["tmp_name"], SITE_ROOT.'\\'.$target_file)) 
     {
